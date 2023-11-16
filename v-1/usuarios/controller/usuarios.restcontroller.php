30dec8965a20c3c849587fda2c9b64b317cddcb1
nayleaA <naygdz306@gmail.com>
2023-11-16 11:18:20 -0600

    me falto subir este 16-11-2023

--- v-1/usuarios/controller/usuarios.restcontroller.php
+++ v-1/usuarios/controller/usuarios.restcontroller.php
@@ -1,7 +1,8 @@
 <?php
 switch ($request_method) {
     case 'GET':
-        if(isset($path_components[$path_index+1]))
+        if(isset($path_components[$path_index+1])){   
+        }
         break;
     case 'POST':
         # code...
